af3b2245a27fe6e826cd8a09432fee491de5a1e6
zonika <zchodosh@conncoll.edu>
2015-10-12 11:39:13 -0400

    the stack is too damn high

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,9 @@
-# Enter your procedural solution here!
+def largest_prime_factor(num)
+  factors = (1..num).to_a.select do |n|
+    n if num%n == 0 && prime?(n)
+  end
+  factors.max
+end
+def prime?(n)
+  (2..Math.sqrt(n)).to_a.none?{|i| n % i == 0} ? true : false
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(num)
+    @num = num
+  end
+  def number
+    factors = (1..@num).to_a.select do |n|
+      n if @num%n == 0 && prime?(n)
+    end
+    factors.max
+  end
+
+  private
+  def prime?(n)
+    (2..Math.sqrt(n)).to_a.none?{|i| n % i == 0} ? true : false
+  end
+end
